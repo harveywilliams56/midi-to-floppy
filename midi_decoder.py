b556a0d9f570044ed1ac8091ac9340296ee2bd48
Harvey Williams <harvey@harvey-ubuntu-pc.(none)>
2014-08-18 21:19:37 +0100

    made a midi decoding file - currently searches for a given string within the file and returns the cursor position

---
+++ midi_decoder.py
@@ -0,0 +1,26 @@
+import binascii as tool #importing module containing hex conversion
+class midi_decoder:
+	def __init__(self):
+		piece = open('minimal.mid')
+		data = piece.read()
+		self.hexi = tool.hexlify(data)
+		self.MTrck = '4d5464'
+		self.notes = []
+	def search(self,data,item,cp): ##class for finding 'item' in text and returning cursor position
+		found = 0
+		item.split ##turns str 'item' into a list of its seperate characters
+		cursor_position = cp
+		for letter in data:
+			if found == len(item)-1:
+				return cursor_position
+			if letter != item[found]:
+				found = 0
+			if letter == item[found]:
+				found += 1
+			cursor_position += 1
+	def run(self):
+		result = self.search(self.hexi,self.MTrck,0)
+		print result
+		
+if __name__ == '__main__':
+	midi_decoder().run()
