c1ba144f80f6569549eb15bd07c6815b1cd6d1ed
Harvey Williams <harvey@harvey-ubuntu-pc.(none)>
2014-08-25 12:31:12 +0100

    Decoder now creates a list of events inside every track

--- midi_decoder.py
+++ midi_decoder.py
@@ -4,42 +4,87 @@ class midi_decoder:
 		piece = open('minimal.mid')
 		data = piece.read()
 		self.hexi = tool.hexlify(data)
-		self.MTrck = '4d54726b'
-		self.tracks = []
 
 
-	def search(self,item,cp): ##class for finding 'item' in text and returning cursor position
+	def search(self,item): ##class for finding 'item' in text and returning cursor position
+		positions = []
 		found = 0
+		cursor_position = 0
 		item.split ##turns str 'item' into a list of its seperate characters
-		cursor_position = cp
 		for letter in self.hexi:
-			if cp == 0:
-				if found == len(item)-1:
-					return cursor_position
-				if letter != item[found]:
-					found = 0
-				if letter == item[found]:
-					found += 1
-				cursor_position += 1
-			else:
-				cp -=1
-		return 'null'
+			cursor_position += 1
+			if letter != item[found]:
+				found = 0
+			if letter == item[found]:
+				found += 1
+			if found == len(item):
+				positions += [cursor_position]
+				found = 0
+		return positions
 
 
 	def run(self):
-		print self.find_tracks()
+		if True:
+			print self.hexi
+			events = []
+			tracks = self.search('4d54726b')
+			end_tracks = self.search('ff2f00')
+			extra = tracks + [len(self.hexi)]
+			for track in range(0,len(tracks)):
+				print track
+				events += [self.find_events(tracks[track],end_tracks[track])]
+				print events
+
+	
+	def return_data(self,start,end):
+		letters = []
+		for letter in self.hexi:
+			start -= 1
+			end -= 1
+			if start <= 0 and end >= 0:
+				letters += letter
+			if end == 0:
+				return letters
 
 
-	def find_tracks(self):
-		more = True
-		cp = 0
-		while more:
-			cp = self.search(self.MTrck,cp)
-			if cp == 'null':
-				return self.tracks
-				more = False
-			else:
-				 self.tracks += [cp]
+	def find_events(self,TrackStart,TrackEnd):
+		vlv = 0
+		events = []
+		cp = TrackStart + 9
+		while True:
+			continuation = True
+			if cp >= TrackEnd:
+				events.pop() ##removes last event, which is an end track
+				return events
+			while continuation:
+				raw_data = self.return_data(cp, cp + 1)
+				value_str = "".join(raw_data)
+				value = int(value_str, 16)
+				if value >= 128:
+					print raw_data
+					vlv += 1
+					cp += 2 
+				else:
+					continuation = False
+			events += ["".join(self.return_data(cp - (2*vlv), cp+7))]
+			cp += 8
+			vlv = 0
+			print events
+			
+	def find_notes(self,TrackStart,TrackEnd):
+		notes = []
+		cp = TrackStart+8
+		while True:
+			value = self.return_data(cp,cp+1)
+			if cp >= TrackEnd or cp == 'null':
+				return notes
+			if value == '9':
+				print cp
+				note = self.return_data(cp+2, cp+4)
+				notes += [note]
+			cp += 6	
+		
+		
 		
 		
 		
